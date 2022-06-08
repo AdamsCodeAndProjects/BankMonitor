cd9621a30241e178d8a972e1ca326ead18cc676b
Adam Januszewski <Adpjan@att.net>
2022-06-08 17:27:30 -0400

    First commit

--- BankMonitor/Login/LoginViewController.swift
+++ BankMonitor/Login/LoginViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    let loginView = LoginView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        style()
+        layout()
     }
+}
 
-
+extension LoginViewController {
+    private func style() {
+        // This is what takes any element and makes it ready for autolayout
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        //  In this array, place a bunch of rules and activate it all at once
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+        
+    }
 }
 
