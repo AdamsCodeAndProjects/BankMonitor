b5ffb05c8f813efe1e7e9351ca7bc3e9a52734a2
Adam Januszewski <Adpjan@att.net>
2022-06-08 12:26:31 -0400

    Initial Commit

---
+++ BankMonitor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankMonitor
+//
+//  Created by adam janusewski on 6/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
